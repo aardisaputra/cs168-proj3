990a8544795cf3693d48441540e553bfd7b78604
Austin Nicola Ardisaputra <ardisaputra.2003@gmail.com>
2024-10-29 23:23:05 -0700

    Idk why 1.3 is not working

--- ext/cs168p2/student_socket.py
+++ ext/cs168p2/student_socket.py
@@ -542,7 +542,11 @@ class StudentUSocket(StudentUSocketBase):
     self.bind(dev.ip_addr, 0)
 
     ## Start of Stage 1.1 ##
-
+    self.snd.nxt = self.snd.iss
+    self.tx(self.new_packet(ack=False, data=None, syn=True))
+    self.state = SYN_SENT
+    self.snd.nxt = self.snd.nxt |PLUS| 1
+    
     ## End of Stage 1.1 ##
 
   def tx(self, p, retxed=False):
@@ -589,6 +593,9 @@ class StudentUSocket(StudentUSocketBase):
     if self.state is CLOSED:
       return
     ## Start of Stage 1.2 ##
+
+    if self.state is SYN_SENT:
+      self.handle_synsent(seg)
 
     ## End of Stage 1.2 ##
     elif self.state in (ESTABLISHED, FIN_WAIT_1, FIN_WAIT_2,
@@ -644,9 +651,15 @@ class StudentUSocket(StudentUSocketBase):
 
     if acceptable_ack:
       ## Start of Stage 1.3 ##
+      self.rcv.nxt = seg.seq |PLUS| 1
+      self.rcv.wnd = seg.win
+      self.snd.una = self.snd.nxt |PLUS| 1
 
       if self.snd.una |GT| self.snd.iss:
-        pass
+        self.snd.nxt = self.snd.nxt |PLUS| 1
+        self.state = ESTABLISHED
+        self.set_pending_ack()
+        self.update_window(seg)
 
       ## End of Stage 1.3 ##
 
